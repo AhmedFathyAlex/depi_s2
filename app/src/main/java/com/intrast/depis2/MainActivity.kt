f5bba97e0c8303148929dacb954dbe440d3ba446
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2025-01-27 19:05:48 +0200

    first commit

---
+++ app/src/main/java/com/intrast/depis2/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.intrast.depis2
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.intrast.depis2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("Name","Ahmed")
+            startActivity(intent)
+//            sendEmail()
+        }
+    }
+    fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_EMAIL, "ahmed@gmail.com")
+        intent.putExtra(Intent.EXTRA_TEXT, "How are you?")
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"No app found",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
