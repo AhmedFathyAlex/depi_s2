3fba08dd9616aab2fed63d959bdd5e6976479c9b
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2025-02-10 19:25:50 +0200

    10/2/2025 - start.

--- app/src/main/java/com/intrast/depis2/ScoreActivity.kt
+++ app/src/main/java/com/intrast/depis2/ScoreActivity.kt
@@ -5,16 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.intrast.depis2.databinding.ActivityScoreBinding
 
 class ScoreActivity : AppCompatActivity() {
+    var teamAScore = 0
+    var teamBScore = 0
+    lateinit var binding: ActivityScoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_score)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.addScoreTeamA.setOnClickListener {
+            scoreGoal(true)
+        }
+        binding.addScoreTeamB.setOnClickListener{
+            scoreGoal(false)
+        }
+        binding.resetButton.setOnClickListener{
+            reset()
+        }
+
+    }
+    fun scoreGoal(isTeamA:Boolean){
+        if(isTeamA){
+            teamAScore++
+            binding.teamAScore.text = teamAScore.toString()
+        }else{
+            teamBScore++
+            binding.teamBScore.text = teamBScore.toString()
+        }
+
+    }
+    fun reset(){
+        teamAScore = 0
+        teamBScore = 0
+        binding.teamAScore.text = teamAScore.toString()
+        binding.teamBScore.text = teamBScore.toString()
+
     }
 }
